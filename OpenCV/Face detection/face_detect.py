091af825ad176e2570842ed5273e432e289662f7
ashutoshd1996 <36852252+ashutoshd1996@users.noreply.github.com>
2022-05-13 10:39:21 +0200

    Face detction 1

--- OpenCV/Face detection/face_detect.py
+++ OpenCV/Face detection/face_detect.py
@@ -8,5 +8,12 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray Person', gray)
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
-cv.waitKey(0)
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 
+print(len(faces_rect))
+
+
+
+
+
+cv.waitKey(0)
